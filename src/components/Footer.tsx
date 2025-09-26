cc91ca3ae3a8667f7d57236d53d631bf1d8eae25
Ozodbek-cloud <vortextblade16@gmail.com>
2025-09-26 18:04:50 +0500

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <header>
+            <section>
+                <nav>
+                    
+                </nav>
+            </section>
+            <section></section>
+        </header>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image'
+import React from 'react'
+import facebook from "../images/ant-design_facebook-filled.png"
+import insta from "../images/ant-design_instagram-filled.png"
+import logo from "../images/1.png"
+import cart from "../images/jam_shopping-cart.png"
+import search from "../images/gg_search.png"
+import person from "../images/Ellipse 8.png"
+
+function Navbar() {
+    return (
+        <div>
+            <header>
+                <section className='bg-container  py-3 bg-[#020202]'>
+                    <div className='container  py-3'>
+                        <nav className='flex justify-between'>
+                            <h1><span className='font-bold text-gray-600'>Mon-Thu:</span> <span className='font-bold text-white'>9:00 AM - 5:30 PM</span></h1>
+                            <h1 className='font-bold text-gray-600'>Visit our showroom in 1234 Street Adress City Address, 1234  <span className='font-bold ml-2 text-white border-b-2 hover:text-blue-700 hover:border-blue-700 transition-all duration-200 cursor-pointer'>Contact Us</span></h1>
+                            <div className='flex justify-center items-center gap-2'>
+                                <h1 className='font-bold text-white'>Call Us: (00) 1234 5678</h1>
+                                <div className='flex gap-1'>
+                                    <Image src={facebook} alt=''></Image>
+                                    <Image src={insta} alt=''></Image>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </section>
+                <section>
+                    <div className='container '>
+                        <nav className='py-5 flex justify-between'>
+                            <div className='flex items-center gap-10'>
+                                <Image src={logo} alt=''></Image>
+                                <div className='flex gap-3'>
+                                    <button className='py-3 cursor-pointer px-6 border-2 border-white rounded-4xl font-bold hover:text-blue-700 hover:border-blue-700 transition-all duration-200'>Laptops</button>
+                                    <button className='py-3 cursor-pointer px-6 border-2 border-white rounded-4xl font-bold hover:text-blue-700 hover:border-blue-700 transition-all duration-200'>Desktop PCs</button>
+                                    <button className='py-3 cursor-pointer px-6 border-2 border-white rounded-4xl font-bold hover:text-blue-700 hover:border-blue-700 transition-all duration-200'>Networking Devices</button>
+                                    <button className='py-3 cursor-pointer px-6 border-2 border-white rounded-4xl font-bold hover:text-blue-700 hover:border-blue-700 transition-all duration-200'>Printers & Scanners</button>
+                                    <button className='py-3 cursor-pointer px-6 border-2 border-white rounded-4xl font-bold hover:text-blue-700 hover:border-blue-700 transition-all duration-200'>PC Parts</button>
+                                    <button className='py-3 cursor-pointer px-6 border-2 border-white rounded-4xl font-bold hover:text-blue-700 hover:border-blue-700 transition-all duration-200'>All Other Products</button>
+                                    <button className='py-3 cursor-pointer px-6 border-2 border-white rounded-4xl font-bold hover:text-blue-700 hover:border-blue-700 transition-all duration-200'>Repairs</button>
+                                    <button className='py-3 cursor-pointer px-6 border-2 rounded-4xl font-bold text-blue-700 border-blue-700  '>Our Deals</button>
+                                </div>
+                            </div>
+                            <div className='flex gap-3 items-center'>
+                                <Image src={search} alt=''></Image>
+                                <Image src={cart} alt=''></Image>
+                                <Image src={person} alt=''></Image>
+                            </div>
+                        </nav>
+                    </div>
+                </section>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar
